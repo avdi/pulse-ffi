ee7403a6678040c5ba49221901a4933a5630e495
Avdi Grimm <avdi@avdi.org>
2013-01-17 16:57:52 -0500

    Use RSpec mocks and stubs.

--- test/pulse_ffi/mainloop_test.rb
+++ test/pulse_ffi/mainloop_test.rb
@@ -1,25 +1,29 @@
 $LOAD_PATH.unshift(File.expand_path('../../../lib', __FILE__))
 require 'minitest/autorun'
 require 'pulse_ffi/mainloop'
-require 'ostruct'
+require 'rspec/mocks'
 
 module PulseFFI
   class MainloopTest < MiniTest::Unit::TestCase
+    def setup
+      RSpec::Mocks.setup(self)
+    end
+
+    def teardown
+      RSpec::Mocks.teardown
+    end
+
     def test_initialize_creates_mainloop
-      api = OpenStruct.new(pa_mainloop_new: :mainloop_ptr)
+      api = double(pa_mainloop_new: :mainloop_ptr)
       loop = Mainloop.new(api: api)
       assert_equal :mainloop_ptr, loop.pointer
     end
 
     def test_run_starts_mainloop
-      api = MiniTest::Mock.new
-      def api.pa_mainloop_new
-        :mainloop_ptr
-      end
+      api = double(pa_mainloop_new: mainloop_ptr = double)
       loop = Mainloop.new(api: api)
-      api.expect(:pa_mainloop_run, nil, [:mainloop_ptr, nil])
+      api.should_receive(:pa_mainloop_run).with(mainloop_ptr, nil)
       loop.run
-      api.verify
     end
   end
 end
